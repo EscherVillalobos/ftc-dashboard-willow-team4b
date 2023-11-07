629c4178663736bd800199a3e96e7ca3642bcf6a
emerlin0 <jack_raines.stu@willowschoolnola.org>
2023-11-07 12:40:27 -0600

    ff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JayrodCode4Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JayrodCode4Bot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,12 +10,16 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp()
+@Config
 public class JayrodCode4Bot extends OpMode {
     public DcMotorEx arm = null;
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
     public Servo leftServo = null;
     public Servo rightServo = null;
+    public static int Top = 100;
+    public static int Down = 0;
+ //fuck u lil bich
 
     @Override
     public void init() {
@@ -27,33 +32,21 @@ public class JayrodCode4Bot extends OpMode {
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
         leftServo = hardwareMap.get(Servo.class, "leftServo");
         rightServo = hardwareMap.get(Servo.class, "rightServo");
+        arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
     }
     public void loop(){
-        arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         int position = arm.getCurrentPosition();
         telemetry.addData("Arm is at", position );
-        if (gamepad1.dpad_up){
-            arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-
-            //arm up code
-            arm.setPower(0.2);
+        if(gamepad1.dpad_up){
+            arm.setTargetPosition(Top);
+            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            arm.setVelocity(300);
             telemetry.addData("Arm", "is up");
-        } else if (gamepad1.dpad_down){
-            arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-
-            //arm down code
-            arm.setPower(-.2);
+        }  if(gamepad1.dpad_down){
+            arm.setTargetPosition(Down);
+            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            arm.setVelocity(300);
             telemetry.addData("Arm", "is down");
-        } else {
-            arm.setPower(0);
-            arm.setTargetPosition(position);
-            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        }
-        if(gamepad1.a){
-            arm.setTargetPosition(200);
-            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-            arm.setPower(0);
-
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic/basic_bot.java
@@ -0,0 +1,132 @@
+package org.firstinspires.ftc.teamcode.basic;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="basic_bot")
+@Config
+public class basic_bot extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotorEx armDrive = null;
+    private Servo leftServo;
+    private Servo rightServo;
+
+    public static int armTarget = 300;
+    public static int armAway = -1;
+    public static int armVelocity = 200;
+
+    public static double leftClawOpen = 1;
+    public static double leftClawClosed = 0.0;
+    public static double rightClawOpen = 1;
+    public static double rightClawClosed = 0.0;
+    public static int armReset = 0;
+    public static int armSpeed = 200;
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        armDrive = hardwareMap.get(DcMotorEx.class, "arm_drive");
+        leftServo = hardwareMap.get(Servo.class, "left_servo");
+        rightServo = hardwareMap.get(Servo.class, "right_servo");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftServo.setDirection(Servo.Direction.FORWARD);
+        rightServo.setDirection(Servo.Direction.REVERSE);
+
+        armDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        waitForStart();
+
+        runtime.reset();
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+
+
+        while (opModeIsActive()) {
+
+
+            double leftPower;
+            double rightPower;
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.left_stick_x;
+
+
+            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+
+
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            if (gamepad1.a) {
+
+                armDrive.setTargetPosition(armTarget);
+
+                armDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                armDrive.setVelocity(armVelocity);
+
+            }
+            else if (gamepad1.x) {
+
+                armDrive.setTargetPosition(armAway);
+
+                armDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                armDrive.setVelocity(armVelocity);
+            }
+            if (gamepad1.b) {
+                armDrive.setTargetPosition(armReset);
+
+                armDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                armDrive.setVelocity(armSpeed);
+            }
+            else if (gamepad1.x) {
+
+                armDrive.setTargetPosition(armAway);
+
+                armDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                armDrive.setVelocity(armVelocity);
+            }
+
+                if (gamepad1.right_bumper) {
+                    leftServo.setPosition(leftClawOpen);
+                    rightServo.setPosition(rightClawOpen);
+
+                }
+                if (gamepad1.left_bumper) {
+                    leftServo.setPosition(leftClawClosed);
+                    rightServo.setPosition(rightClawClosed);
+                }
+                telemetry.addData("velocity", armDrive.getVelocity());
+                telemetry.addData("encoderValue", armDrive.getCurrentPosition());
+
+                telemetry.update();
+
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+                telemetry.update();
+            }
+    }
+    }
